fe213efbbd29df4486f7a6cd2cad5c923f854fd5
ChillerDragon <chillerdragon@gmail.com>
2026-04-06 10:22:09 +0200

    fix assert order

--- spec/convert_test.lua
+++ spec/convert_test.lua
@@ -2,20 +2,20 @@
 local assert_eq = require("spec.simple_assert").assert_eq
 require("../src/card_converter")
 
-assert_eq(card_to_str({ suite = "clubs", rank = 14 }), "🃑")
-assert_eq(card_to_str({ suite = "spades", rank = 2 }), "🂢")
-assert_eq(card_to_str({ suite = "hearts", rank = 4 }), "🂴")
-assert_eq(card_to_str({ suite = "diamonds", rank = 4 }), "🃄")
-assert_eq(card_to_str({ suite = "clubs", rank = 4 }), "🃔")
-assert_eq(card_to_str({ suite = "clubs", rank = 14 }), "🃑")
-assert_eq(card_to_str({ suite = "clubs", rank = 2 }), "🃒")
+assert_eq("🃑", card_to_str({ suite = "clubs", rank = 14 }))
+assert_eq("🂢", card_to_str({ suite = "spades", rank = 2 }))
+assert_eq("🂴", card_to_str({ suite = "hearts", rank = 4 }))
+assert_eq("🃄", card_to_str({ suite = "diamonds", rank = 4 }))
+assert_eq("🃔", card_to_str({ suite = "clubs", rank = 4 }))
+assert_eq("🃑", card_to_str({ suite = "clubs", rank = 14 }))
+assert_eq("🃒", card_to_str({ suite = "clubs", rank = 2 }))
 
-assert_eq(str_to_card("🃒").rank, 2)
-assert_eq(str_to_card("🃒").suite, "clubs")
+assert_eq(2, str_to_card("🃒").rank)
+assert_eq("clubs", str_to_card("🃒").suite)
 
-assert_eq(str_to_card("🃔").rank, 4)
-assert_eq(str_to_card("🃔").suite, "clubs")
+assert_eq(4, str_to_card("🃔").rank)
+assert_eq("clubs", str_to_card("🃔").suite)
 
-assert_eq(str_to_card("🃑").rank, 14)
-assert_eq(str_to_card("🃑").suite, "clubs")
+assert_eq(14, str_to_card("🃑").rank)
+assert_eq("clubs", str_to_card("🃑").suite)
 
