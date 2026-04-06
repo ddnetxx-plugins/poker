de1f36f33c7f8a6c7002f4a04524857ba9a7df4e
ChillerDragon <chillerdragon@gmail.com>
2026-04-06 09:37:36 +0200

    Fix another

--- spec/position_test.lua
+++ spec/position_test.lua
@@ -6,7 +6,7 @@ ddnetpp = require("spec.mock.ddnetpp")
 ddnetpp.chat.silent = true
 ddnetpp.verbosity = 0
 
-local game = Poker:new(nil, { x = 33, y = 30 })
+local game = Poker:new(nil, { x = 33, y = 30 }, 8)
 game:join_table(0) -- co
 game:join_table(1) -- button
 game:join_table(2) -- small blind
@@ -20,14 +20,14 @@ game:new_game()
 -- first player that joins will get the button
 -- but it moves on the start of the first round
 -- so the actual first button is the second joiner
-assert_eq(false, game.players[0].is_button)
-assert_eq(true, game.players[1].is_button)
+assert_eq(false, game:find_player(0).is_button)
+assert_eq(true, game:find_player(1).is_button)
 
-assert_eq(ButtonOffset.BUTTON, game.players[1].position.offset)
-assert_eq(ButtonOffset.SMALL_BLIND, game.players[2].position.offset)
-assert_eq(ButtonOffset.BIG_BLIND, game.players[3].position.offset)
-assert_eq(ButtonOffset.UTG, game.players[4].position.offset)
-assert_eq(ButtonOffset.UTG+1, game.players[5].position.offset)
-assert_eq(ButtonOffset.UTG+2, game.players[6].position.offset)
-assert_eq(ButtonOffset.UTG+3, game.players[7].position.offset)
+assert_eq(ButtonOffset.BUTTON, game:find_player(1).position.offset)
+assert_eq(ButtonOffset.SMALL_BLIND, game:find_player(2).position.offset)
+assert_eq(ButtonOffset.BIG_BLIND, game:find_player(3).position.offset)
+assert_eq(ButtonOffset.UTG, game:find_player(4).position.offset)
+assert_eq(ButtonOffset.UTG+1, game:find_player(5).position.offset)
+assert_eq(ButtonOffset.UTG+2, game:find_player(6).position.offset)
+assert_eq(ButtonOffset.UTG+3, game:find_player(7).position.offset)
 
