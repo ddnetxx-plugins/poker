8e1a06a35d44ac9c9d398fcc330eb29a19d79a76
ChillerDragon <chillerdragon@gmail.com>
2026-04-08 16:26:11 +0200

    found issue w tests

--- spec/hands_test.lua
+++ spec/hands_test.lua
@@ -53,3 +53,39 @@ assert_eq("two pair", hand.name)
 assert_eq("🂺🃊🂤🃄🃑", hand.cards)
 assert_eq("tens and fours", hand.description)
 assert_eq(201008200, hand.score)
+
+hand = find_best_hand(
+	{ "🂤", "🂺" },
+	{ "🃄", "🃊", "🃛", "🃑", "🂻" }
+)
+assert_eq("two pair", hand.name)
+assert_eq("🃛🂻🂺🃊🃑", hand.cards)
+assert_eq("jacks and tens", hand.description)
+assert_eq(201114200, hand.score)
+
+hand = find_best_hand(
+	{ "🂤", "🂩" },
+	{ "🃄", "🂹", "🃛", "🃑", "🂻" }
+)
+assert_eq("two pair", hand.name)
+assert_eq("🃛🂻🂩🂹🃑", hand.cards)
+assert_eq("jacks and nines", hand.description)
+assert_eq(201113200, hand.score)
+
+hand = find_best_hand(
+	{ "🂤", "🂨" },
+	{ "🃄", "🂸", "🃛", "🃑", "🂻" }
+)
+assert_eq("two pair", hand.name)
+assert_eq("🃛🂻🂨🂸🃑", hand.cards)
+assert_eq("jacks and eigths", hand.description)
+assert_eq(201112200, hand.score)
+
+hand = find_best_hand(
+	{ "🂣", "🂩" },
+	{ "🂳", "🂹", "🃛", "🃂", "🂻" }
+)
+assert_eq("two pair", hand.name)
+assert_eq("🃛🂻🂩🂹🂣", hand.cards)
+assert_eq("jacks and nines", hand.description)
+assert_eq(201109900, hand.score) -- FIXME: this score is LOWER than eights wtf thats wrong
