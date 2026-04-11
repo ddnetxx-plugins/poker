189f63167d27e923fb6707783b9f2c7bdfdae30e
ChillerDragon <chillerdragon@gmail.com>
2026-04-11 11:14:19 +0200

    not happy with the test but it passes xd

--- spec/blind_all_in_test.lua
+++ spec/blind_all_in_test.lua
@@ -33,20 +33,24 @@ t.set_hole_cards(game, 3, "🃋🃛")
 
 game:player_action(0, { action = "call" })
 game:player_action(1, { action = "call" })
+t.assert_eq(GameState.PRE_FLOP, game.state)
 game:player_action(2, { action = "call" })
+-- we alreday flop before player 3 did anything
+-- that is because that player was put all in
+-- by the blind
+t.assert_eq(GameState.FLOP, game.state)
+
 game:player_action(3, { action = "check" })
-
--- FIXME: this test is work in progress
---        but before blind all in i wanted to implement
---        a regular all in next to act test
-
 -- TODO: double check if this should be printed
 --       premove success is silent because its broadcasted to all
 --       should premove errors be printed then?
--- t.assert_eq("You are already all in, wait until next round", ddnetpp.get_chat_line(3, -1))
+t.assert_eq("You are already all in, wait until next round", ddnetpp.get_chat_line(3, -1))
 
--- t.assert_eq(GameState.FLOP, game.state)
+t.all_check(game)
+t.assert_eq(GameState.TURN, game.state)
 
--- t.all_check_call_till_showdown_and_rig_board(game, "🂤🂴🃄🃔🃕")
--- t.assert_eq("You won the entire pot with 400 chips in it!", ddnetpp.get_chat_line(0, -2))
--- t.assert_eq("'mock0' won with best hand four of a kind (quad fours)", ddnetpp.get_chat_line(0, -1))
+t.all_check(game)
+t.assert_eq(GameState.RIVER, game.state)
+
+t.all_check(game)
+t.assert_eq(GameState.PRE_FLOP, game.state)
