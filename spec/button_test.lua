d2a60c29f1aa0afedc8d73deca925f635043702c
ChillerDragon <chillerdragon@gmail.com>
2026-04-12 11:56:47 +0200

    OH SHIT WHAT dealer=sb in headsup???
    apparently button is first to act pre flop and second to act post flop? woah
    
    bro why is it second after the flop? if the dealer is the sb
    inst the first to act after the flop the player after the big blind????
    which in this case is the sb?????

--- spec/button_test.lua
+++ spec/button_test.lua
@@ -1,4 +1,4 @@
-local assert_eq = require("simple.assert").assert_eq
+local t = require("spec.util.test_base")
 require("../src/poker")
 ddnetpp = require("spec.mock.ddnetpp")
 
@@ -13,31 +13,31 @@ game:new_game()
 -- first player that joins will get the button
 -- but it moves on the start of the first round
 -- so the actual first button is the second joiner
-assert_eq(false, game:find_player(0).is_button)
-assert_eq(true, game:find_player(1).is_button)
+t.assert_eq(false, game:find_player(0).is_button)
+t.assert_eq(true, game:find_player(1).is_button)
 
 game:player_action(0, { action = "call" })
 game:player_action(1, { action = "check" })
 
 -- after one round of betting the button should
 -- NOT have moved yet
-assert_eq(GameState.FLOP, game.state)
-assert_eq(false, game:find_player(0).is_button)
-assert_eq(true, game:find_player(1).is_button)
+t.assert_eq(GameState.FLOP, game.state)
+t.assert_eq(false, game:find_player(0).is_button)
+t.assert_eq(true, game:find_player(1).is_button)
 
-assert_eq(GameState.FLOP, game.state)
+t.assert_eq(GameState.FLOP, game.state)
 game:player_action(0, { action = "check" })
 game:player_action(1, { action = "check" })
-assert_eq(GameState.TURN, game.state)
+t.assert_eq(GameState.TURN, game.state)
 
 game:player_action(0, { action = "check" })
 game:player_action(1, { action = "check" })
 
 -- also on the river the button should not have moved
 -- NOT have moved yet
-assert_eq(GameState.RIVER, game.state)
-assert_eq(false, game:find_player(0).is_button)
-assert_eq(true, game:find_player(1).is_button)
+t.assert_eq(GameState.RIVER, game.state)
+t.assert_eq(false, game:find_player(0).is_button)
+t.assert_eq(true, game:find_player(1).is_button)
 
 game:player_action(0, { action = "check" })
 game:player_action(1, { action = "check" })
@@ -45,23 +45,32 @@ game:player_action(1, { action = "check" })
 -- both players checked on the river the round is over
 -- we are pre flop in the next round
 -- NOW we expect the button to have moved to the next player
-assert_eq(GameState.PRE_FLOP, game.state)
-assert_eq(true, game:find_player(0).is_button)
-assert_eq(false, game:find_player(1).is_button)
+t.assert_eq(GameState.PRE_FLOP, game.state)
+t.assert_eq(true, game:find_player(0).is_button)
+t.assert_eq(false, game:find_player(1).is_button)
 
-game:player_action(0, { action = "check" })
+t.assert_eq(1, game:next_to_act().client_id)
 game:player_action(1, { action = "call" })
-
 game:player_action(0, { action = "check" })
+t.assert_eq(GameState.FLOP, game.state)
+
+game:player_action(1, { action = "check" })
+game:player_action(0, { action = "check" })
+t.assert_eq(GameState.TURN, game.state)
+
+game:player_action(1, { action = "check" })
+game:player_action(0, { action = "check" })
+t.assert_eq(GameState.RIVER, game.state)
+
 game:player_action(1, { action = "check" })
 
-game:player_action(0, { action = "check" })
-game:player_action(1, { action = "check" })
 
+t.assert_eq(0, game:next_to_act().client_id)
 game:player_action(0, { action = "check" })
-game:player_action(1, { action = "check" })
+
+-- t.assert_eq("", ddnetpp.get_chat_line(0, -1))
 
 -- another button move
-assert_eq(GameState.PRE_FLOP, game.state)
-assert_eq(false, game:find_player(0).is_button)
-assert_eq(true, game:find_player(1).is_button)
+t.assert_eq(GameState.PRE_FLOP, game.state)
+t.assert_eq(false, game:find_player(0).is_button)
+t.assert_eq(true, game:find_player(1).is_button)
